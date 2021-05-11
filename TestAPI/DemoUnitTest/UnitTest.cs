c959def6b5fe72b6eed49ab887f71428ad883732
Federico Damis <fdamis@hipotecario.com.ar>
2021-05-11 16:30:05 -0300

    Se agregan Unit Tests de prueba

---
+++ TestAPI/DemoUnitTest/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+namespace DemoUnitTest
+{
+    [TestFixture]
+    public class UnitTests
+    {        
+        [Test]
+        public void TestGetApi()
+        {
+            string searchString = "Roberto";
+            WebRequest request = WebRequest.Create("http://localhost:63527/api/personas/" + searchString);
+            WebResponse response = request.GetResponse();
+            string responseJSONString = null;
+
+            using (Stream dataStream = response.GetResponseStream())
+            {                
+                StreamReader reader = new StreamReader(dataStream);
+                responseJSONString = reader.ReadToEnd();             
+            }
+
+            dynamic responseJSON = JsonConvert.DeserializeObject<dynamic>(responseJSONString);
+            string foundName = responseJSON["Datos"][0]["nombre"];           
+            response.Close();
+            Assert.AreEqual(searchString, foundName);
+        }        
+    }
+}
